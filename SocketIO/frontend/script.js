62a6196730109f68fb8e9944a5b0aade65075161
Satwik Bhardwaj <sat12bhardwaj@gmail.com>
2019-06-29 00:05:49 +0530

    unsuccesful attempt for chat room

--- SocketIO/frontend/script.js
+++ SocketIO/frontend/script.js
@@ -1,10 +1,15 @@
 let socket = io();
-
+socket.on('connected', () => {
+    console.log("Connected " + socket.id)
+    })
 
 $(function () {
-    let msglist = $('#msglist')
-    let sendbtn = $('#sendmsg')
-    let msgbox = $('#msgbox')
+    let msglist1 = $('#mssgList1')
+    let sendbtn1 = $('#sendmsg1')
+    let msgbox1 = $('#msgbox1')
+    let msglist2 = $('#messages2')
+    let sendbtn2 = $('#sendmsg2')
+    let msgbox2 = $('#msgbox2')
     let loginbox = $('#loginbox')
     let loginbtn = $('#loginbtn')
     let loginDiv = $('#login-div')
@@ -12,18 +17,23 @@ $(function () {
 
     let user = ''
 
-    socket.on('connected', () => {
-        console.log("Connected " + socket.id)
-               
-    })
+    
     socket.on("new_user", function(data){
         //online notification
-        msglist.append($('<li>' + data +" is online" + '</li>'))
+        msglist1.append($("<li>"+ data +" is online" + "</li>"))
+        msglist2.append($("<li>"+ data +" is online" + "</li>"))
     })
-    sendbtn.click(function () {
-        socket.emit('send_msg', {
+    sendbtn1.click(function () {
+        socket.emit('send_msg1', {
             user: user,
-            message: msgbox.val()
+            message: msgbox1.val()
+        })
+    })
+
+    sendbtn2.click(function () {
+        socket.emit('send_msg2', {
+            user: user,
+            message: msgbox2.val()
         })
     })
 
@@ -31,12 +41,21 @@ $(function () {
         user = loginbox.val()
         chatDiv.show()
         loginDiv.hide()
+        
         socket.emit('login', {
-            user: user
+            user: user,
+            Room1: "Room1",
+            Room2: "Room2"
         })
+               
+        
     })
 
-    socket.on('recv_msg', function (data) {
-        msglist.append($('<li>' + data.user + ': ' + data.message + '</li>'))
+    socket.on('recv_msg1', function (data) {
+        msglist1.append($("<li>" + data.user + ': ' + data.message + "</li>" ))
+    })
+
+    socket.on('recv_msg2', function (data) {
+        msglist2.append($("<li>" + data.user + ': ' + data.message + "</li>" ))
     })
 })
--- SocketIO/server.js
+++ SocketIO/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const socketio = require('socket.io')
 const http = require('http')
-const SERVER_PORT = process.env.PORT || 2345
+const SERVER_PORT = process.env.PORT || 1234
 
 const app = express();
 const server = http.createServer(app)
@@ -14,30 +14,55 @@ app.use('/', express.static(path.join(__dirname, 'frontend')))
 
 io.on('connection', (socket) => {
     console.log("New socket formed from " + socket.id)
-    socket.emit('connected')
+    socket.emit('connected')    
+    
+
+    /*socket.on("Room1", function(room){
+        
+    })*/
 
     socket.on('login', (data) => {
         // username is in data.user
-        usersockets[data.user] = socket.id
-        socket.broadcast.emit("new_user",data.user)//for signalling the connection of new user
+        usersockets[socket.id] = data.user
+        //for signalling the connection of new user
+        //socket.broadcast.emit("new_user",data.user)
         console.log(usersockets)
+        socket.join(data.Room1);
+        socket.join(data.Room2);
+        socket.broadcast.in(data.Room1).emit("new_user", data.user);
+        socket.broadcast.in(data.Room2).emit("new_user", data.user);
     })
     
-    socket.on('send_msg', (data) => {
+    socket.on('send_msg1', (data) => {
         // if we use io.emit, everyone gets it
         // if we use socket.broadcast.emit, only others get it
         if (data.message.startsWith('@')) {
             //data.message = "@a: hello"
             // split at :, then remove @ from beginning
             let recipient = data.message.split(':')[0].substr(1)
-            let rcptSocket = usersockets[recipient]
+            let rcptSocket = recipient;
             //send message to all
-            io.to(rcptSocket).emit('recv_msg', data)
+            io.to(rcptSocket).emit('recv_msg1', data)
         } else {
-            io.emit('recv_msg', data)            
+            io.sockets.in("Room1").emit('recv_msg1', data)            
+        }
+    })
+
+    socket.on('send_msg2', (data) => {
+        // if we use io.emit, everyone gets it
+        // if we use socket.broadcast.emit, only others get it
+        if (data.message.startsWith('@')) {
+            //data.message = "@a: hello"
+            // split at :, then remove @ from beginning
+            let recipient = data.message.split(':')[0].substr(1)
+            let rcptSocket = recipient;
+            //send message to all
+            io.to(rcptSocket).emit('recv_msg2', data)
+        } else {
+            io.sockets.in("Room2").emit('recv_msg2', data)            
         }
     })
 
 })
 
-server.listen((SERVER_PORT), () => console.log('Website open on http://localhost:2345'))
+server.listen((SERVER_PORT), () => console.log('Website open on http://localhost:1234'))
